9c8cf1bca122a7fe3bddbf4b1e97623d6238ab3a
r-zk <161754145+r-zk@users.noreply.github.com>
2024-09-27 12:17:09 +0400

    Add files via upload

---
+++ 27092024.c
@@ -0,0 +1,29 @@
+  #include <stdio.h>		// give access to the stdio.h library //
+
+
+  int main(){
+
+    //defining variables//
+
+    #define admin_password "coffeemakersimulator"   //adminstrator password defined//
+
+    #define total_amount_espresso "3.5 AED"     // espresso price defined//
+    #define total_amount_cappucino "4.5 AED"    //cappaucino price defined//
+    #define total_amount_mocha "5.5 AED"    // mocha price defined//
+
+    #define espresso_beans_grams 8    //espresso beans amount in grams defined//
+    #define cappuccino_beans_grams 8    //cappuccino beans amount in grams defined//
+    #define mocha_beans_grams 8         //mocha beans amoount in grams defined//
+
+    #define espresso_water_millilitres 30   //espresso water amount in milligrams defined//
+    #define cappuccino_water_millilitres 30     //cappuccino water amount in milligrams defined//
+    #define mocha_water_millilitres 39  //mocha water amount in milligrams defined//
+
+    #define espresso_milk_millilitres 0 
+    #define cappuccino_milk_millilitres 70
+    #define mocha_milk_millilitres 160
+
+
+
+
+  }
